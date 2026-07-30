b8a4d8801c9644020a26b939c8440b0e602750a7
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-07-30 19:30:45 +0500

    now working on sending emails

--- workers/extract_email.py
+++ workers/extract_email.py
@@ -3,14 +3,11 @@ from fastapi import HTTPException, status
 import pandas as pd
 from models.upload_file import Upload
 from database import SessionLocal
-from services.upload_file import  get_upload_by_campaign
-from services.campaigns import  get_campaign_by_id
-
 from models.campaigns import Campaign
+from models.campaign_recipients import CampaignRecipient
 from schema.campaigns import CampaignStatus
 from schema.upload_file import UploadStatus
-from schema.campaign_recipients import AddRecipientsSchema
-from sqlalchemy.orm import Session
+# from schema.campaign_recipients import AddRecipientsSchema
 
 @celery.task()
 def extract_emails(upload_id : int):
@@ -27,7 +24,7 @@ def extract_emails(upload_id : int):
             if upload is None:
                 raise HTTPException(
                       status_code=status.HTTP_400_BAD_REQUEST,
-                      details="FIle doesn't exist"
+                      detail="FIle doesn't exist"
                 )
             extension = upload.file_path.split(".")[-1].lower()
             if extension == "xlsx":
@@ -40,10 +37,13 @@ def extract_emails(upload_id : int):
                         detail="Only XLSX and CSV files are allowed"
                     )
             if "email" not in df.columns:
-                  raise HTTPException("Email column is required")
+                  raise HTTPException(
+                        status_code=status.HTTP_400_BAD_REQUEST,
+                        detail="Email column is required"
+                  )
 
-            for rows in df.itterows():
-                recipient = AddRecipientsSchema(
+            for rows in df.iterows():
+                recipient = CampaignRecipient(
                     campaign_id = upload.campaign_id,
                     upload_id = upload.id,
                     name = rows["name"],
@@ -52,14 +52,14 @@ def extract_emails(upload_id : int):
                     phone = rows["phone"],
                     is_valid_email=True
                 )
-                db.add(recipient)
+                db.bulk_save_objects(recipient)
 
                 upload.total_records = len(df)
                 upload.processed_records = 0
                 upload.status = UploadStatus.COMPLETED
 
                 campaign = (
-                      db.query(Campaign).filter(Campaign.id == upload.id).first()
+                      db.query(Campaign).filter(Campaign.id == upload.campaign_id).first()
                 )
                 campaign.status = CampaignStatus.READY
 
