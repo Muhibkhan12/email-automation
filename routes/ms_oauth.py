924bba27064983914deca1004f6cc3eeae58a802
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-08-03 21:46:06 +0500

    redirect functionality completed

--- routes/ms_oauth.py
+++ routes/ms_oauth.py
@@ -1,4 +1,21 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, status
+from fastapi.responses import RedirectResponse 
 from sqlalchemy.orm import Session
 
+from services.oauth_service import OAuthService
+
 from database import SessionLocal
+
+router = APIRouter(
+    prefix="/api/oauth/outlook",
+    tags=["OAuth"]
+)
+
+@router.get('/connect')
+def connect_outlook():
+    auth_url = OAuthService.generate_auth_url()
+
+    return RedirectResponse(
+        url=auth_url,
+        status_code=status.HTTP_302_FOUND
+    )
