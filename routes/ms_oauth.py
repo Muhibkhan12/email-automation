51a108b002c3af8b8fa285c498c6c2c77f32b89d
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-08-04 20:16:49 +0500

    I WILL NEVER RECOMMEND MS OAUTH AGAIN TO ANYONE. THIS SHIT IS SCARY AS HELL

--- routes/ms_oauth.py
+++ routes/ms_oauth.py
@@ -14,14 +14,10 @@ router = APIRouter(
 @router.get('/connect')
 def connect_outlook():
     auth_url = OAuthService.generate_auth_url()
-
-
-    
-
-    # return{
-    #     "auth_url" : auth_url
-    # }
-    return RedirectResponse(
-        url=auth_url,
-        status_code=status.HTTP_302_FOUND
-    )
+    return{
+        "auth_url" : auth_url
+    }
+    # return RedirectResponse(
+    #     url=auth_url,
+    #     status_code=status.HTTP_302_FOUND
+    # )
