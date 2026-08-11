8cb4ae936a2275396c7cd85e8194c83643302be4
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-08-11 18:40:50 +0500

    STILL WORKING ON DASHBOARD

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import Sidebar from './Sidebar'
 
 const Dashboard = () => {
   return (
-    <>
-        <Sidebar/>
-        <div>Dashboard</div>
-    </>
+    <div>Dashboard</div>
   )
 }
 
--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,13 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+
 import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
-import Dashboard from '../pages/Dashboard'
-import Sidebar from '../pages/Sidebar'
-import { Route, Routes } from 'react-router-dom'
 
 const AppRouter = () => {
   return (
     <Routes>
-        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path="/" element={<Dashboard/>} />
     </Routes>
   )
 }
