962084333b2fafc4268a10cb6bc8d734f5fdc453
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-08-21 19:18:42 +0500

    need to debug CampaignContext

--- frontend/src/contexts/CampaignContext.tsx
+++ frontend/src/contexts/CampaignContext.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import { useContext,useState,useEffect } from 'react'
+import { getCampaign } from '../services/CampaignService'
 
-const CampaignContext = () => {
-  return (
-    <div>CampaignContext</div>
-  )
+export const campaignContext = createContext("null");
+
+export const campaignProvider = ({children}) =>{
+    const [campaign, setCampaign] = useState([])
+
+    useEffect(() => {
+        const fetchCampaign = async ()=>{
+            const data = await getCampaign();
+
+            setCampaign(data);
+        }
+        fetchCampaign();
+    },[]);
+
+    return (
+        <campaignContext.Provider value={campaigns}>
+            {children}
+        </campaignContext.Provider>  
+    );
+    
+
 }
-
-export default CampaignContext
