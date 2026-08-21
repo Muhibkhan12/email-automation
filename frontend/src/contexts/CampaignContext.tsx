3a5ad2b61be85d6b7e171e3a91a39694d867b03a
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-08-21 19:09:49 +0500

    campaign context

---
+++ frontend/src/contexts/CampaignContext.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useContext,useState,useEffect } from 'react'
+
+const CampaignContext = () => {
+  return (
+    <div>CampaignContext</div>
+  )
+}
+
+export default CampaignContext
