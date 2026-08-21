66695fd50e738d1024193205670c410e4c1c0e1a
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-08-21 21:55:07 +0500

    campaign fetch by id

--- frontend/src/services/CampaignService.tsx
+++ frontend/src/services/CampaignService.tsx
@@ -6,3 +6,8 @@ export const getCampaign = async() =>{
     const response = await axios.get("/campaigns/");
     return response.data;
 };
+
+export const getCampaignById = async() =>{
+    const response = await axios.get("/campaigns/{1}");
+    return response.data;
+}
