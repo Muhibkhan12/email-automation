ec92a6557ab4915d25b9bea0931498c75b9296d5
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-08-24 20:24:32 +0500

    working on routes

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -24,38 +24,41 @@ import AdminQueueMonitor from "../pages/Admin/AdminQueuemonitor";
 import AdminSettings from "../pages/Admin/AdminSettings";
 import AdminSenderAccounts from "../pages/Admin/AdminSenderAccount";
 
-import { Ad } from "lucide-react";
-
 const AppRouter = () => {
   return (
     <Routes>
-        <Route path="/" element={<Dashboard/>} />
-        <Route path="/notifications" element={<Notifications/>} />
-        <Route path="/emaillogs" element={< EmailLogs/>}/>
-        <Route path="/templates" element={<Templates/> }/>
-        <Route path="/settings" element={<Settings/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/sender-account" element={<SenderAccount/>} />
-        <Route path="/queuemonitor" element={<QueueMonitor/>}/>
-        <Route path="/campaign" element={<Campaign/>}/>
-        <Route path="/upload" element={<Upload/>} />
-        <Route path="/analytics" element={<Analytics/>}/>
-        <Route path="/recipients" element={<Recipients/>}/>
-        <Route path="/login"  element={<Login/>}/>
-        <Route path="/register"  element={<Register/>}/>
+
+        <Route path="/user">
+          <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/notifications" element={<Notifications/>} />
+          <Route path="/emaillogs" element={< EmailLogs/>}/>
+          <Route path="/templates" element={<Templates/> }/>
+          <Route path="/settings" element={<Settings/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/sender-account" element={<SenderAccount/>} />
+          <Route path="/queuemonitor" element={<QueueMonitor/>}/>
+          <Route path="/campaign" element={<Campaign/>}/>
+          <Route path="/upload" element={<Upload/>} />
+          <Route path="/analytics" element={<Analytics/>}/>
+          <Route path="/recipients" element={<Recipients/>}/>
+          <Route path="/login"  element={<Login/>}/>
+          <Route path="/register"  element={<Register/>}/>
+        </Route>
+
 
         {/* admin routes */}
-
-        <Route path="/admin/dashboard"  element={<AdminDashboard/>} />
-        <Route path="/admin/analytics"  element={<AdminAnalytics/>} />
-        <Route path="/admin/templates" element={<EmailTemplatesAdmin/>}/>
-        <Route path="/admin/campaigns" element={<AdminCampaigns/>}/>
-        <Route path="/admin/users" element={<AdminUsers/>}/>
-        <Route path="/admin/notifications" element={<AdminNotifications/>}/>
-        <Route path="/admin/emaillogs" element={<AdminEmailLogs/>}/>
-        <Route path="/admin/queuemonitor" element={<AdminQueueMonitor/>}/>
-        <Route path="/admin/settings" element={<AdminSettings/>} />
-        <Route path="/admin/senders-account" element={<AdminSenderAccounts/>} />
+        <Route path="/admin">
+          <Route path="/dashboard"  element={<AdminDashboard/>} />
+          <Route path="/analytics"  element={<AdminAnalytics/>} />
+          <Route path="/templates" element={<EmailTemplatesAdmin/>}/>
+          <Route path="/campaigns" element={<AdminCampaigns/>}/>
+          <Route path="/users" element={<AdminUsers/>}/>
+          <Route path="/notifications" element={<AdminNotifications/>}/>
+          <Route path="/emaillogs" element={<AdminEmailLogs/>}/>
+          <Route path="/queuemonitor" element={<AdminQueueMonitor/>}/>
+          <Route path="/settings" element={<AdminSettings/>} />
+          <Route path="/senders-account" element={<AdminSenderAccounts/>} />
+        </Route>
     </Routes>
   )
 }
