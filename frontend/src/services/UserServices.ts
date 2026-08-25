8257e920943b7f44d198c6bf5cbc08331de6344d
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-08-25 21:22:16 +0500

    done for today

--- frontend/src/services/UserServices.ts
+++ frontend/src/services/UserServices.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 import type { User, RegisterUser, UserLogin, UpdateUser  } from "../types/UserTypes";
 export const getAllUser = async() => {
-    const response = await axios.get('/auth/users');
+    const response = await axios.get<User[]>('/auth/users');
     return response.data
 }
 export const getUsersById = async(id : number) => {
-    const response = await axios.get(`/auth/user/${id}`)
+    const response = await axios.get<User>(`/auth/user/${id}`)
     return response.data
 }
 export const updateUser = async(id : number, data : UpdateUser) => {
-    const response = await axios.post(`/auth/user/${id}`)
+    const response = await axios.post<User>(`/auth/user/${id}`)
     return response.data
 }
