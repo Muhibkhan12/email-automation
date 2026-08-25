585f13647772f4c35f2945867017283084910696
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-08-25 19:36:14 +0500

    fk issue fixed

--- backend/models/html_templates.py
+++ backend/models/html_templates.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String,Integer,Text,DateTime, Boolean
+from sqlalchemy import String,Integer,Text,DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime, UTC
 from database import Base
@@ -9,6 +9,7 @@ class HTMLTemplate(Base):
     __tablename__ = "html_templates"
 
     id : Mapped[int] = mapped_column(Integer, primary_key=True,index=True)
+    user_id : Mapped[int] = mapped_column(Integer, ForeignKey("user.id"), nullable=False)
     name : Mapped[str] = mapped_column(String(255), nullable=False)
     html_content : Mapped[str] = mapped_column(Text,nullable=False)
     description: Mapped[str | None] = mapped_column(String(255),nullable=True)
