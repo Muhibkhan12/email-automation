b9abe952e1c3f1f5f98819c0a871002571c35b45
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-08-27 21:03:03 +0500

    send acc schema changed

--- backend/schema/sender_account.py
+++ backend/schema/sender_account.py
@@ -1,17 +1,20 @@
 from pydantic import BaseModel, EmailStr
 
 class addSenderAccountSchema(BaseModel):
-    user_id: int
     email : EmailStr
-    password : str
     display_name : str
-    username : str
-    smtp_host : str
-    smtp_port : int
     provider : str
+    access_token : str
+    refresh_token : str
+    token_expires_at : str
     daily_limit : int
     hourly_limit : int
-
+    emails_sent_today : int
+    status : str
+    created_at : str
+    updated_at : str
+    emails_sent_hour : int
+    
 class SenderAccountResponse(BaseModel):
     email : EmailStr	
     display_name : str	
@@ -23,11 +26,8 @@ class SenderAccountResponse(BaseModel):
 
 class updateSenderAccout(BaseModel):
     email : EmailStr
-    password : str
     display_name : str
     username : str
-    smtp_host : str
-    smtp_port : int
     provider : str
     daily_limit : int
     hourly_limit : int
