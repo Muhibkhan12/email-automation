6df057ea83f19f29c4a6011059997abf791fe44e
MuhibKhan12 <muhibkhan2299@gmail.com>
2026-09-01 21:06:06 +0500

    done for today

--- frontend/src/services/AuthServices.tsx
+++ frontend/src/services/AuthServices.tsx
@@ -1,7 +1,17 @@
 import axios from "axios"
-import type{ AuthResponse,UserLogin, RegisterUser} from "../types/UserTypes"
+import type{ User, AuthResponse,UserLogin, RegisterUser} from "../types/UserTypes"
 
 export const loginUser = async(data : UserLogin): Promise<AuthResponse> => {
   const response = await axios.post('/auth/login', data);
   return response.data
 };
+
+export const UserRegister = async(data: RegisterUser) => {
+  const response = await axios.post('/auth/register',data);
+  return response.data;
+}
+
+export const getProfile = async():Promise<User> =>{
+  const response = await axios.get('/auth/profile');
+  return response.data
+}
